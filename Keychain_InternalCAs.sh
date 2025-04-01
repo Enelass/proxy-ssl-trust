986ee77b1fa46c9433cddde2a2dbee43d1ec3a59
Florian Bidabe <Florian.Bidabe@cba.com.au>
2025-04-01 22:52:41 +1100

    Syntax & Logging improvement +  addition of Intermediate CAs scanning

--- Keychain_InternalCAs.sh
+++ Keychain_InternalCAs.sh
@@ -12,14 +12,22 @@
 #               not using the MacOS system certificate store (Keychain Manager).   #
 #  INITIAL RELEASE DATE: 19-Sep-2024                                               #
 #  AUTHOR: Florian Bidabe                                                          #
-#  LAST RELEASE DATE: 19-Sep-2024                                                  #
-#  VERSION: 0.2                                                                    #
-#  REVISION:                                                                       #
+#  LAST RELEASE DATE: 01-Apr-2025                                                  #
+#  VERSION: 0.3                                                                    #
+#  REVISION: Syntax & Logging improvement +  addition of Intermediate CAs scanning #
 #                                                                                  #
 #                                                                                  #
 ####################################################################################
-version=0.2
 
+################################# Variables ########################################
+version=0.3
+
+
+# If this is a standalone execution...
+if [[ -z ${logI-} ]]; then clear; source ./stderr_stdout_syntax.sh; source ./play.sh; fi
+
+
+#################################  Functions ########################################
 # Function to display the Help Menu
 help() {
     clear
@@ -35,21 +43,29 @@ help() {
     exit 0
 }
 
-# Function to remove stdout and stderr
+# Function to remove stdout and stderr for every cert but the summary of it
 quiet() {
     quiet=1
     exec 3>&1 4>&2
     exec 1>/dev/null 2>&1
 }
 
+# Function to add back stdout and stderr
 unquiet() {
     exec 1>&3 2>&4
     exec 3>&- 4>&-
 }
 
+# Function to remove stdout and stderr for everything...
 silent() {
     quiet; silent=1
 }
+
+intermediate() {
+    intermediate=1
+}
+
+
 
 ###########################   Script SWITCHES   ###########################
 # Switches and Executions for the initial call (regardless of when)
@@ -58,6 +74,7 @@ while [[ $# -gt 0 ]]; do
     --help|-h) help ;;
     --quiet|-q) quiet ;;
     --silent|-q) silent ;;
+    --intermediate|-i) intermediate ;;
    *) ;;
   esac
   shift
@@ -87,71 +104,58 @@ if [[ -z "${teefile-}" ]]; then
 
     # Function to check if we're running on MacOS
     get_macos_version() { local product_name=$(sw_vers -productName); local product_version=$(sw_vers -productVersion); local build_version=$(sw_vers -buildVersion); echo "$product_name $product_version ($build_version)" }
-
-    # Function to check if a command exists
-    command_exists() { command -v "$1" >/dev/null 2>&1 }
-
-    # Function to check if a package is installed via Homebrew
-    brew_package_installed() { brew list | grep -qE "^$1$" }
-
-    # Function to attempt installing a package 3x times via Homebrew
-    attempt_install() {
-        local package="$1"; local attempts=2
-        while [ $attempts -gt 0 ]; do
-            sudo -u "$user" brew install "$package" > /dev/null
-            if [ $? -eq 0 ]; then
-                log "$package installed successfully."
-                return 0
-            fi
-            log "Error   - Failed to install $package. Attempts left: $attempts"
-            attempts=$((attempts - 1))
-        done
-        handle_error "Error   - Failed to install $package after 2 attempts. Exiting..."
-        exit 1
-    }
 fi
 
-
-
-### Requirement:
-log "Info    - Verifying System requirements..."
-# Check if the operating system is Darwin (macOS)
-if [ "$(uname)" != "Darwin" ]; then log "Error   - This Script is meant to run on macOS, not on: $(uname -v)"; exit 1; fi
-log "Info    -      Running on $(get_macos_version)"
-
-# 1. Function to export a list of Internal certificates from the MacOS Keychain Manager
-keychainCA() {
+# 1. Function to export a list of Internal Root CAs from the MacOS Keychain Manager
+keychainRootCA() {
+    local CAList
     output=$(security find-certificate -a | grep 'labl' | grep -v "com.apple")
-    unset IntCAList; log "Info    - Inspecting MacOS Keychain to find internal Root Certificate Authorities with ability to sign other certificates..." 
+    unset CAList; log "Info    - Inspecting MacOS Keychain to find internal Root Certificate Authorities with ability to sign other certificates..." 
     echo '-------------------------------------------------------------------------------------------'
     while read -r line; do # Reading one certificate at the time and verifying if it is a RootCA or not... (Intermediate CA and signed certificates are excluded)
         currentcertname=$(echo $line | cut -d\" -f4)
         currentcertsubj=$(security find-certificate -c "$currentcertname" | grep '"subj"<blob>')
         currentcertissu=$(security find-certificate -c "$currentcertname" | grep '"issu"<blob>')
         if [[ ${currentcertsubj#*=} == ${currentcertissu#*=} ]]; then
-            log "Info    - \""$currentcertname"\" is not signed by any other certificates, it is either self-signed or is a RootCA"
-            #log "Info    - Verifying if \""$currentcertname"\" is a "signing" RootCA..."
-            if is_root_ca "$currentcertname"; then IntCAList+="$currentcertname"$'\n'; fi
+            logI "\""$currentcertname"\" is not signed by any other certificates, it is either self-signed or is a RootCA"
+            if is_signing_ca "$currentcertname"; then CAList+="$currentcertname"$'\n'; fi
         else
-            log "Info    - Certificate: \""$currentcertname"\" is not a RootCA, it's signed by another issuer then itself. This could be a certificate or Intermediate CA..."
+            logI "Certificate: \""$currentcertname"\" is not a RootCA, it's signed by another issuer then itself. This could be a certificate or Intermediate CA..."
         fi
         echo '-------------------------------------------------------------------------------------------'
     done <<< "$output"
 
     # Removing duplicate from the signing Root CA certificates list
-    IntCAList=$(echo $IntCAList | sort | uniq) 
+    CAList=$(echo $CAList | sort | uniq) 
 
-    # We want to see the output
-    if [[ -n "${quiet}" && -z "${silent-}" ]]; then
-        unquiet
-    fi
-    IntCAList=$(echo "$IntCAList" | grep -v '^\s*$') # Remove empty lines...
-    echo -e "-------------------------------------------------------------------------------------------\n\nThe below is a list of Internal Root CAs with the ability to sign other certificate:\nThese are most likely used to sign internal servers or to perform SSL Forward Inspection (MITM)\n\n$IntCAList"
+    # We want to see the summary
+    CAList=$(echo "$CAList" | grep -v '^\s*$') # Remove empty lines...
+    if [[ -n "${quiet}" && -z "${silent-}" ]]; then unquiet; fi
+    echo -e "-------------------------------------------------------------------------------------------\n\nThe below is a list of Internal Root CAs with the ability to sign other certificate:\nThese are most likely used to sign internal servers or to perform SSL Forward Inspection (MITM)\n\n$CAList"    
 }
 
+keychainIntCA() {
+    output=$(security find-certificate -a | grep 'labl' | grep -v "com.apple")
+    unset CAList; logI "Inspecting MacOS Keychain to find internal Certificate Authorities (Intermediate and Root) with ability to sign other certificates..." 
+    echo '-------------------------------------------------------------------------------------------'
+    while read -r line; do # Reading one certificate at the time and verifying if it is a RootCA or not... (Intermediate CA and signed certificates are excluded)
+        currentcertname=$(echo $line | cut -d\" -f4)
+        currentcertsubj=$(security find-certificate -c "$currentcertname" | grep '"subj"<blob>')
+        currentcertissu=$(security find-certificate -c "$currentcertname" | grep '"issu"<blob>')
+        if is_signing_ca "$currentcertname"; then CAList+="$currentcertname"$'\n'; fi
+    done <<< "$output"
 
-# 2. Function to check if a certificate is a signing Root CA
-function is_root_ca {
+    # Removing duplicate from the signing Root CA certificates list
+    CAList=$(echo $CAList | sort | uniq)
+
+    # We want to see the output
+    CAList=$(echo "$CAList" | grep -v '^\s*$') # Remove empty lines...
+    if [[ -n "${quiet}" && -z "${silent-}" ]]; then unquiet; fi
+    echo -e "-------------------------------------------------------------------------------------------\n\nThe below is a list of Internal Root CAs with the ability to sign other certificate:\nThese are most likely used to sign internal servers or to perform SSL Forward Inspection (MITM)\n\n$CAList"
+}
+
+# 2. Function to check if a certificate is a Signing CA
+function is_signing_ca {
     local base64_cert=$1
     # Extract Basic Constraints and Key Usage extensions
     local basic_constraints=$(openssl x509 -in <(security find-certificate -c "$base64_cert" -p) -text -noout | grep -A 1 "X509v3 Basic Constraints" | grep 'CA:TRUE')
@@ -160,19 +164,29 @@ function is_root_ca {
     if [[ "$basic_constraints" == *"CA:TRUE"* ]]; then
         # Check if Certificate Sign is present in Key Usage
         if [[ "$key_usage" == *"Certificate Sign"* ]]; then
-            log "Info    - $base64_cert has X509v3 Basic Constraints set to CA:TRUE and Key Usage set to Certificate Sign. It is a \"signing\" Root CA"
+            logS "$base64_cert has X509v3 Basic Constraints set to CA:TRUE and Key Usage set to Certificate Sign. It is a \"signing\" CA"
             return 0
         else
-            log "Info    - $base64_cert has X509v3 Basic Constraints set to CA:TRUE but Key Usage is \"not\" set to Certificate Sign. It isn't a signing Root CA"
+            logI "$base64_cert has X509v3 Basic Constraints set to CA:TRUE but Key Usage is \"not\" set to Certificate Sign. It isn't a signing CA"
             return 1
         fi
     else
-        log "Info    - $base64_cert does not have X509v3 Basic Constraints set to CA:TRUE, hence it isn't a RootCA"
+        logI "$base64_cert does not have X509v3 Basic Constraints set to CA:TRUE, hence it isn't a Certificate Authority"
         return 1
     fi
 }
 
-keychainCA
-if [[ -n "${silent-}" ]]; then
-    unquiet
+
+### Requirement:
+logI "Verifying System requirements..."
+# Check if the operating system is Darwin (macOS)
+if [ "$(uname)" != "Darwin" ]; then log "Error   - This Script is meant to run on macOS, not on: $(uname -v)"; exit 1; fi
+logI "     Running on $(get_macos_version)"
+
+if [[ -z ${intermediate-} ]]; then
+    keychainRootCA      # Root CAs only
+else
+    keychainIntCA       # Root and Intermediate CAs
 fi
+
+if [[ -n "${silent-}" ]]; then unquiet; fi
