a88c79d295d4ffce17f7250a34f8b7f93e7036a6
Florian Bidabe <Florian.Bidabe@cba.com.au>
2025-04-01 15:48:34 +1100

    update

--- FixMePlease.sh
+++ FixMePlease.sh
@@ -2,8 +2,8 @@
 
 
 # Download all this crap in /tmp
+echo "Downloading all the required scripts to fix SSL and Proxy issues. Please wait..."
 cd /tmp
-sleep 10
 curl -sk -L -O "https://github.com/Enelass/proxy-ssl-trust/archive/refs/heads/main.zip"
 unzip -q -o main.zip
 cd proxy-ssl-trust-main
